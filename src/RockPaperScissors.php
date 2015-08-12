02ff6c2540f05658dd8b8f75482b513aba85e53a
Phillip Shannon & Micah Courey <student@epicodus.com>
2015-08-12 10:25:15 -0700

    Create functionality for lowercasing inputs.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,6 +3,9 @@
     {
         function playRockPaperScissors($input1, $input2)
         {
+            $input1 = strtolower($input1);
+            $input2 = strtolower($input2);
+
             if (($input1 == "rock") && ($input2 == "scissors")) {
               return "Player one";
             }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -18,6 +18,21 @@
             //Assert
             $this->assertEquals("Player one", $result);
         }
+
+        function test_input_case()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "ROCK";
+            $input2 = "sCiSsors";
+
+            //Act
+            $result = $test_RockPaperScissors->playRockPaperScissors($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player one", $result);
+        }
+
     }
 
 ?>
