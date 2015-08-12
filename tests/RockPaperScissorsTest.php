b910642a7b52c5c3c109700e35cb955aa34b8fa5
Phillip Shannon & Micah Courey <student@epicodus.com>
2015-08-12 10:07:53 -0700

    add test file and composer.json

---
+++ tests/RockPaperScissorsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "src/RockPaperScissors.php";
+
+    class RockPaperScissorsTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_rock_scissors()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "rock";
+            $input2 = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->playRockPaperScissors($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player one", $result);
+        }
+    }
+
+?>
