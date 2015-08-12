2e40583833b61f0e8dec2fd64843069c1f00c409
Phillip Shannon & Micah Courey <student@epicodus.com>
2015-08-12 10:21:11 -0700

    add player one 'rock' player two 'scissors' functionality

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,7 +3,9 @@
     {
         function playRockPaperScissors($input1, $input2)
         {
-            
+            if (($input1 == "rock") && ($input2 == "scissors")) {
+              return "Player one";
+            }
         }
     }
 ?>
