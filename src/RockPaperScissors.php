aee2e1811e0f1905b092a7010c2de750b018d279
Phillip Shannon & Micah Courey <student@epicodus.com>
2015-08-12 10:40:59 -0700

    Create player one scissors player two paper scenario.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -7,7 +7,11 @@
             $input2 = strtolower($input2);
 
             if (($input1 == "rock") && ($input2 == "scissors")) {
-              return "Player one";
+                return "Player one";
+            } elseif (($input1 == "scissors") && ($input2 == "paper")) {
+                return "Player one";
+            } else {
+                return "Tie";
             }
         }
     }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -33,6 +33,20 @@
             $this->assertEquals("Player one", $result);
         }
 
+        function test_scissors_paper()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "scissors";
+            $input2 = "paper";
+
+            //Act
+            $result = $test_RockPaperScissors->playRockPaperScissors($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player one", $result);
+        }
+
     }
 
 ?>
